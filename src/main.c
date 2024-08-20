785c862c7ef8e29c6570f40171580baded2c23b9
Barsy-Barsevich <victor.kinderknecht7@gmail.com>
2024-08-20 14:52:47 +0500

    update

---
+++ src/main.c
@@ -0,0 +1,176 @@
+#include "mik32_hal_spi.h"
+#include "sd.h"
+#include "mik32_hal_usart.h"
+#include "xprintf.h"
+
+/*
+ * Данный пример демонстрирует работу с SPI в режиме ведущего.
+ * Ведущий передает и принимает от ведомого на выводе CS0 20 байт.
+ *
+ * Результат передачи выводится по UART0.
+ * Данный пример можно использовать совместно с ведомым из примера HAL_SPI_Slave.
+ */
+
+USART_HandleTypeDef husart0;
+SD_Descriptor_t sd;
+static uint8_t sd_buffer[512];
+
+
+void SystemClock_Config(void);
+static void USART_Init();
+static void GPIO_Init();
+void DelayUs(uint32_t us);
+
+int main()
+{
+    SystemClock_Config();
+
+    USART_Init();
+
+    GPIO_Init();
+    HAL_GPIO_WritePin(GPIO_2, GPIO_PIN_7, 0);
+
+    sd.voltage = SD_Voltage_from_3_2_to_3_3;
+    xprintf("\nResult if init: Status: %u; Type: %u\n\n", SD_Init(&sd), sd.type);
+
+    HAL_DelayMs(1000);
+
+    HAL_GPIO_WritePin(GPIO_2, GPIO_PIN_7, 1);
+    // xprintf("Reading sector 9: Status: %u\n", SD_SingleRead(&sd, 9, sd_buffer));
+    // for (uint16_t i=0; i<512; i+=16)
+    // {
+    //     xprintf("%04X: ", i);
+    //     for (uint8_t j=0; j<16; j++)
+    //     {
+    //         xprintf(" %02X", sd_buffer[i+j]);
+    //     }
+    //     xprintf("\n");
+    // }
+
+    // for (uint16_t i=0; i<512; i++) sd_buffer[i] = i & 0xFF;
+
+    // xprintf("Writing sector 9: Status: %u\n", SD_SingleWrite(&sd, 9, sd_buffer));
+
+    // xprintf("Reading sector 9: Status: %u\n", SD_SingleRead(&sd, 9, sd_buffer));
+    // for (uint16_t i=0; i<512; i+=16)
+    // {
+    //     xprintf("%04X: ", i);
+    //     for (uint8_t j=0; j<16; j++)
+    //     {
+    //         xprintf(" %02X", sd_buffer[i+j]);
+    //     }
+    //     xprintf("\n");
+    // }
+
+
+
+    for (uint8_t k=0; k<10; k++)
+    {
+        xprintf("Reading sector %u: Status: %u\n", k, SD_SingleRead(&sd, k, sd_buffer));
+        for (uint16_t i=0; i<512; i+=16)
+        {
+            xprintf("%04X: ", i);
+            for (uint8_t j=0; j<16; j++)
+            {
+                xprintf(" %02X", sd_buffer[i+j]);
+            }
+            xprintf("\n");
+        }
+    }
+}
+
+void SystemClock_Config(void)
+{
+    PCC_InitTypeDef PCC_OscInit = {0};
+
+    PCC_OscInit.OscillatorEnable = PCC_OSCILLATORTYPE_ALL;
+    PCC_OscInit.FreqMon.OscillatorSystem = PCC_OSCILLATORTYPE_OSC32M;
+    PCC_OscInit.FreqMon.ForceOscSys = PCC_FORCE_OSC_SYS_UNFIXED;
+    PCC_OscInit.FreqMon.Force32KClk = PCC_FREQ_MONITOR_SOURCE_OSC32K;
+    PCC_OscInit.AHBDivider = 0;
+    PCC_OscInit.APBMDivider = 0;
+    PCC_OscInit.APBPDivider = 0;
+    PCC_OscInit.HSI32MCalibrationValue = 128;
+    PCC_OscInit.LSI32KCalibrationValue = 128;
+    PCC_OscInit.RTCClockSelection = PCC_RTC_CLOCK_SOURCE_AUTO;
+    PCC_OscInit.RTCClockCPUSelection = PCC_CPU_RTC_CLOCK_SOURCE_OSC32K;
+    HAL_PCC_Config(&PCC_OscInit);
+}
+
+void DelayUs(uint32_t time_us)
+{
+    time_us >>= 3;
+    register uint32_t del = 14 * (HAL_PCC_GetSysClockFreq() / (PM->DIV_AHB+1) / 1000UL) / 16000;
+    //register uint32_t del = 10 * (HAL_PCC_GetSysClockFreq() / (PM->DIV_AHB+1) / 1000UL) / 32000;
+    asm volatile(
+        "beq    %[count], x0, end_metka_%="         "\n\t"
+        "cycle_main_%=:"
+        "mv     t0, %[del]"                         "\n\t"
+        "cycle_internal_%=:"
+        "addi   t0, t0, -1"                         "\n\t"
+        "bne    t0, x0, cycle_internal_%="          "\n\t"
+        "addi   %[count], %[count], -1"             "\n\t"
+        "bne    %[count], x0, cycle_main_%="        "\n\t"
+        "end_metka_%=:"
+        ::
+        [count] "r" (time_us), [del] "r" (del) : "t0"
+    );
+}
+
+
+static void USART_Init()
+{
+    husart0.Instance = UART_0;
+    husart0.transmitting = Enable;
+    husart0.receiving = Disable;
+    husart0.frame = Frame_8bit;
+    husart0.parity_bit = Disable;
+    husart0.parity_bit_inversion = Disable;
+    husart0.bit_direction = LSB_First;
+    husart0.data_inversion = Disable;
+    husart0.tx_inversion = Disable;
+    husart0.rx_inversion = Disable;
+    husart0.swap = Disable;
+    husart0.lbm = Disable;
+    husart0.stop_bit = StopBit_1;
+    husart0.mode = Asynchronous_Mode;
+    husart0.xck_mode = XCK_Mode3;
+    husart0.last_byte_clock = Disable;
+    husart0.overwrite = Disable;
+    husart0.rts_mode = AlwaysEnable_mode;
+    husart0.dma_tx_request = Disable;
+    husart0.dma_rx_request = Disable;
+    husart0.channel_mode = Duplex_Mode;
+    husart0.tx_break_mode = Disable;
+    husart0.Interrupt.ctsie = Disable;
+    husart0.Interrupt.eie = Disable;
+    husart0.Interrupt.idleie = Disable;
+    husart0.Interrupt.lbdie = Disable;
+    husart0.Interrupt.peie = Disable;
+    husart0.Interrupt.rxneie = Disable;
+    husart0.Interrupt.tcie = Disable;
+    husart0.Interrupt.txeie = Disable;
+    husart0.Modem.rts = Disable; //out
+    husart0.Modem.cts = Disable; //in
+    husart0.Modem.dtr = Disable; //out
+    husart0.Modem.dcd = Disable; //in
+    husart0.Modem.dsr = Disable; //in
+    husart0.Modem.ri = Disable;  //in
+    husart0.Modem.ddis = Disable;//out
+    husart0.baudrate = 115200;
+    HAL_USART_Init(&husart0);
+}
+
+
+static void GPIO_Init()
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    __HAL_PCC_GPIO_0_CLK_ENABLE();
+    __HAL_PCC_GPIO_1_CLK_ENABLE();
+    __HAL_PCC_GPIO_2_CLK_ENABLE();
+    GPIO_InitStruct.Pin = GPIO_PIN_7;
+    GPIO_InitStruct.Mode = HAL_GPIO_MODE_GPIO_OUTPUT;
+    GPIO_InitStruct.Pull = HAL_GPIO_PULL_NONE;
+    HAL_GPIO_Init(GPIO_2, &GPIO_InitStruct);
+}
---
+++ src/sd.c
@@ -0,0 +1,289 @@
+#include "sd.h"
+
+static void SPI0_Init(void)
+{
+    hspi0.Instance = SPI_0;
+
+    /* Режим SPI */
+    hspi0.Init.SPI_Mode = HAL_SPI_MODE_MASTER;
+
+    /* Настройки */
+    hspi0.Init.CLKPhase = SPI_PHASE_ON;
+    hspi0.Init.CLKPolarity = SPI_POLARITY_HIGH;
+    hspi0.Init.ThresholdTX = 4;
+
+    /* Настройки для ведущего */
+    hspi0.Init.BaudRateDiv = SPI_BAUDRATE_DIV256;
+    hspi0.Init.Decoder = SPI_DECODER_NONE;
+    hspi0.Init.ManualCS = SPI_MANUALCS_ON;
+    hspi0.Init.ChipSelect = SPI_CS_0;
+
+    if (HAL_SPI_Init(&hspi0) != HAL_OK)
+    {
+        xprintf("SPI_Init_Error\n");
+    }
+}
+
+
+void SD_SendCommand(SD_Commands_enum command, uint32_t operand, uint8_t crc, uint8_t* resp)
+{
+    uint8_t data = 0xFF;
+    //HAL_SPI_CS_Enable(&hspi0, SPI_CS_0);
+    do
+    {
+        HAL_SPI_Exchange(&hspi0, &data, resp, 1, SPI_TIMEOUT_DEFAULT);
+    } while (*resp != 0xFF);
+    data = (uint8_t)command;
+    HAL_SPI_Exchange(&hspi0, &data, resp, 1, SPI_TIMEOUT_DEFAULT);
+    data = ((operand>>24) & 0xFF);
+    HAL_SPI_Exchange(&hspi0, &data, resp, 1, SPI_TIMEOUT_DEFAULT);
+    data = ((operand>>16) & 0xFF);
+    HAL_SPI_Exchange(&hspi0, &data, resp, 1, SPI_TIMEOUT_DEFAULT);
+    data = ((operand>>8) & 0xFF);
+    HAL_SPI_Exchange(&hspi0, &data, resp, 1, SPI_TIMEOUT_DEFAULT);
+    data = (operand & 0xFF);
+    HAL_SPI_Exchange(&hspi0, &data, resp, 1, SPI_TIMEOUT_DEFAULT);
+    data = crc;
+    HAL_SPI_Exchange(&hspi0, &data, resp, 1, SPI_TIMEOUT_DEFAULT);
+    // data = 0xFF;
+    // HAL_SPI_Exchange(&hspi0, &data, resp, 1, SPI_TIMEOUT_DEFAULT);
+    // data = 0xFF;
+    // HAL_SPI_Exchange(&hspi0, &data, resp, 1, SPI_TIMEOUT_DEFAULT);
+    data = 0xFF;
+    do
+    {
+        HAL_SPI_Exchange(&hspi0, &data, resp, 1, SPI_TIMEOUT_DEFAULT);
+    } while (*resp == 0xFF);
+    
+    /* R7 response */
+    if ((command == CMD8) || (command == CMD58))
+    {
+        for (uint8_t i=0; i<4; i++)
+            HAL_SPI_Exchange(&hspi0, &data, resp+i+1, 1, SPI_TIMEOUT_DEFAULT);
+    }
+
+    //HAL_SPI_CS_Disable(&hspi0);
+    xprintf("Com: 0x%X; R1: %08b", command, resp[0]);
+    if ((command == CMD8) || (command == CMD58))
+    {
+        uint32_t ocr = ((uint32_t)resp[1]<<24 | (uint32_t)resp[2]<<16 |
+                        (uint32_t)resp[3]<<8 | resp[4]);
+        xprintf(", extra: %032b", ocr);
+    }
+    xprintf("\n");
+}
+
+
+
+
+SD_Status_enum SD_Init(SD_Descriptor_t* param)
+{
+    SPI0_Init();
+    HAL_SPI_CS_Disable(&hspi0);
+    HAL_DelayMs(100);
+    //80 тактов на линии SCK
+    uint8_t data = 0xFF;
+    uint8_t dummy;
+    for (uint8_t i=0; i<10; i++)
+        HAL_SPI_Exchange(&hspi0, &data, &dummy, 1, SPI_TIMEOUT_DEFAULT);
+
+    uint8_t resp[5];
+
+    HAL_SPI_CS_Enable(&hspi0, SPI_CS_0);
+
+    SD_SendCommand(CMD0, 0, 0x95, resp);
+
+    SD_SendCommand(CMD8, 0x1AA, 0x87, resp);
+    
+    /* It is v1 SD-card or not-SD-card */
+    if (resp[0] & SD_R1_ILLEGAL_COMMAND_M)
+    {
+        SD_SendCommand(CMD58, 0, 0xFF, resp);
+        uint32_t ocr = ((uint32_t)resp[1]<<24 | (uint32_t)resp[2]<<16 |
+                        (uint32_t)resp[3]<<8 | resp[4]);
+        if (!(param->voltage & ocr))
+        {
+            HAL_SPI_CS_Disable(&hspi0);
+            return SD_IncorrectVoltage;
+        }
+        if (resp[0] & SD_R1_ILLEGAL_COMMAND_M)
+        {
+            HAL_SPI_CS_Disable(&hspi0);
+            return SD_UnknownCard;
+        }
+        uint8_t counter = 200;
+
+        /* Trying to send ACMD41 */
+        SD_SendCommand(CMD55, 0, 0xFF, resp);
+        SD_SendCommand(ACMD41, 0x40000000, 0xFF, resp);
+
+        /* It is a MMC */
+        if (resp[0] & SD_R1_ILLEGAL_COMMAND_M)
+        {
+            /* >74 clock cycles on SCK */
+            for (uint8_t i=0; i<10; i++) 
+                HAL_SPI_Exchange(&hspi0, &data, &dummy, 1, SPI_TIMEOUT_DEFAULT);
+            /* Go from idle_mode */
+            while (resp[0] & SD_R1_IDLE_STATE_M)
+            {
+                SD_SendCommand(CMD1, 0, 0xFF, resp);
+                counter -= 1;
+                if (counter == 0)
+                {
+                    HAL_SPI_CS_Disable(&hspi0);
+                    return SD_TimeoutError;
+                }
+            }
+            param->type = MMC;
+            HAL_SPI_CS_Disable(&hspi0);
+            return SD_OK;
+        }
+        /* It is a SDv1 */
+        else
+        {
+            /* Go from idle_mode */
+            while (resp[0] & SD_R1_IDLE_STATE_M)
+            {
+                SD_SendCommand(CMD55, 0, 0xFF, resp);
+                SD_SendCommand(ACMD41, 0x40000000, 0xFF, resp);
+                counter -= 1;
+                if (counter == 0)
+                {
+                    HAL_SPI_CS_Disable(&hspi0);
+                    return SD_TimeoutError;
+                }
+            }
+            param->type = SDv1;
+            HAL_SPI_CS_Disable(&hspi0);
+            return SD_OK;
+        }
+    }
+
+    /* It is SD v2, SDHC or SDXC card */
+    else
+    {
+        /* check the check_pattern */
+        if (resp[4] != 0xAA)
+        {
+            HAL_SPI_CS_Disable(&hspi0);
+            return SD_CommunicationError;
+        }
+
+        /* Check the card's valid voltage */
+        SD_SendCommand(CMD58, 0, 0xFF, resp);
+        uint32_t ocr = ((uint32_t)resp[1]<<24 | (uint32_t)resp[2]<<16 |
+                        (uint32_t)resp[3]<<8 | resp[4]);
+        if (!(param->voltage & ocr))
+        {
+            HAL_SPI_CS_Disable(&hspi0);
+            return SD_IncorrectVoltage;
+        }
+
+        /* >74 clock cycles on SCK */
+            for (uint8_t i=0; i<10; i++) 
+                HAL_SPI_Exchange(&hspi0, &data, &dummy, 1, SPI_TIMEOUT_DEFAULT);
+
+        /* Go from idle_mode */
+        uint8_t counter = 200;
+        while (resp[0] & SD_R1_IDLE_STATE_M)
+        {
+            SD_SendCommand(CMD55, 0, 0xFF, resp);
+            SD_SendCommand(ACMD41, 0x40000000, 0xFF, resp);
+            counter -= 1;
+            if (counter == 0)
+            {
+                HAL_SPI_CS_Disable(&hspi0);
+                return SD_TimeoutError;
+            }
+        }
+
+        /* Read the CCS value */
+        SD_SendCommand(CMD58, 0, 0xFF, resp);
+        uint8_t ccs = resp[1] & 0b01000000;
+        if (ccs == 0) param->type = SDv2;
+        else param->type = SDHC;
+        HAL_SPI_CS_Disable(&hspi0);
+        return SD_OK;
+    }
+    
+    // while(1)
+    // {
+    //     SD_SendCommand(CMD55, 0, 0xFF);
+    //     SD_SendCommand(ACMD41, 0x40000000, 0xFF);
+    // }
+    //     HAL_DelayMs(100);
+
+    // while (1)
+    // {
+    // // HAL_SPI_CS_Enable(&hspi0, SPI_CS_0);
+
+    // // data = 0xFF;
+    // // HAL_SPI_Exchange(&hspi0, &data, &dummy, 1, SPI_TIMEOUT_DEFAULT);
+    // // data = 0x40;
+    // // HAL_SPI_Exchange(&hspi0, &data, &dummy, 1, SPI_TIMEOUT_DEFAULT);
+    // // data = 0x00;
+    // // HAL_SPI_Exchange(&hspi0, &data, &dummy, 1, SPI_TIMEOUT_DEFAULT);
+    // // data = 0x00;
+    // // HAL_SPI_Exchange(&hspi0, &data, &dummy, 1, SPI_TIMEOUT_DEFAULT);
+    // // data = 0x00;
+    // // HAL_SPI_Exchange(&hspi0, &data, &dummy, 1, SPI_TIMEOUT_DEFAULT);
+    // // data = 0x00;
+    // // HAL_SPI_Exchange(&hspi0, &data, &dummy, 1, SPI_TIMEOUT_DEFAULT);
+    // // data = 0x95;
+    // // HAL_SPI_Exchange(&hspi0, &data, &dummy, 1, SPI_TIMEOUT_DEFAULT);
+    // // data = 0xFF;
+    // // HAL_SPI_Exchange(&hspi0, &data, &dummy, 1, SPI_TIMEOUT_DEFAULT);
+    // // data = 0xFF;
+    // // HAL_SPI_Exchange(&hspi0, &data, &dummy, 1, SPI_TIMEOUT_DEFAULT);
+    // // HAL_SPI_CS_Disable(&hspi0);
+    // //     xprintf("R1: %08b\n", dummy);
+    // //     HAL_DelayMs(100);
+    // }
+}
+
+
+SD_Status_enum SD_SingleRead(SD_Descriptor_t* local, uint32_t addr, uint8_t* buf)
+{
+    uint8_t resp, dummy = 0xFF;
+    HAL_SPI_CS_Enable(&hspi0, SPI_CS_0);
+    SD_SendCommand(CMD17, addr, 0xff, &resp);
+    // for (uint16_t i=0; i<512; i++)
+    //     HAL_SPI_Exchange(&hspi0, &dummy, &resp, 1, SPI_TIMEOUT_DEFAULT);
+    if (resp != 0) return resp;
+    uint16_t counter = 0;
+    while ((resp != 0xFE) && (resp != 0xFC))
+    {
+        HAL_SPI_Exchange(&hspi0, &dummy, &resp, 1, SPI_TIMEOUT_DEFAULT);
+        if (counter >= SD_SREAD_WAIT_ATTEMPTS) return SD_TimeoutError;
+        counter += 1;
+        if (resp == 0xFF) continue;
+    }
+    for (uint16_t i=0; i<512; i++)
+    {
+        HAL_SPI_Exchange(&hspi0, &dummy, buf+i, 1, SPI_TIMEOUT_DEFAULT);
+    }
+    HAL_SPI_Exchange(&hspi0, &dummy, &resp, 1, SPI_TIMEOUT_DEFAULT);
+    HAL_SPI_Exchange(&hspi0, &dummy, &resp, 1, SPI_TIMEOUT_DEFAULT);
+    HAL_SPI_CS_Disable(&hspi0);
+    return SD_OK;
+}
+
+
+SD_Status_enum SD_SingleWrite(SD_Descriptor_t* local, uint32_t addr, uint8_t* buf)
+{
+    uint8_t resp, dummy = 0xFF;
+    HAL_SPI_CS_Enable(&hspi0, SPI_CS_0);
+    SD_SendCommand(CMD24, addr, 0xff, &resp);
+    if (resp != 0) return resp;
+    HAL_SPI_Exchange(&hspi0, &dummy, &resp, 1, SPI_TIMEOUT_DEFAULT);
+    dummy = 0xFC;
+    HAL_SPI_Exchange(&hspi0, &dummy, &resp, 1, SPI_TIMEOUT_DEFAULT);
+    for (uint16_t i=0; i<512; i++)
+    {
+        HAL_SPI_Exchange(&hspi0, buf+i, &resp, 1, SPI_TIMEOUT_DEFAULT);
+    }
+    dummy = 0xFF;
+    HAL_SPI_Exchange(&hspi0, &dummy, &resp, 1, SPI_TIMEOUT_DEFAULT);
+    HAL_SPI_Exchange(&hspi0, &dummy, &resp, 1, SPI_TIMEOUT_DEFAULT);
+    return SD_OK;
+}
