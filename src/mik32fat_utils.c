0c109dc7b9231a59f90e58454a9d3582c3247728
Barsy-Barsevich <victor.kinderknecht7@gmail.com>
2025-09-06 02:59:21 +0500

    ls bug fixing

--- src/mik32fat_utils.c
+++ src/mik32fat_utils.c
@@ -7,7 +7,6 @@
 MIK32FAT_Status_TypeDef mik32fat_utils_ls(MIK32FAT_Descriptor_TypeDef *fs, FILE *output)
 {
     MIK32FAT_TempData_TypeDef temp = fs->temp;
-    uint32_t sector = fs->data_region_begin + fs->temp.cluster * fs->param.sec_per_clust;
     uint32_t entires_per_sector = fs->sector_len_bytes / sizeof(MIK32FAT_Entire_TypeDef);
     char name[20];
     MIK32FAT_Status_TypeDef res = MIK32FAT_STATUS_OK;
@@ -26,7 +25,8 @@ MIK32FAT_Status_TypeDef mik32fat_utils_ls(MIK32FAT_Descriptor_TypeDef *fs, FILE 
             for (int j=0; j<entires_per_sector; j++)
             {
                 uint8_t first_name_sym = ((uint8_t*)entire[j].Name)[0];
-                if (first_name_sym != 0xE5 && first_name_sym != 0x00)
+                if (first_name_sym == 0x00) break;
+                if (first_name_sym != 0xE5)
                 {
                     mik32fat_name_read_from_entire(entire+j, name);
                     fprintf(output, "%06b %s\n", entire[j].Attr, name);
